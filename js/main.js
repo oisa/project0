0e0ad19f47a41be38995f85257e674d779104aa4
oisa <imucup@gmail.com>
2021-01-05 18:49:05 +1100

    Further customisations

--- js/main.js
+++ js/main.js
@@ -2,57 +2,116 @@
 
 ///////////////////////////// GLOBAL VARIABLES /////////////////////////////////
 
-  let player1 = {
-    name: 'Player 1',
-    symbol: 'x',
-    score: 0
+let player1 = {
+  name: 'Player 1',
+  symbol: 'x',
+  score: 0
+}
+
+let player2 = {
+  name: 'Player 2',
+  symbol: 'o',
+  score: 0
+}
+
+let gameStatus = ['', '', '', '', '', '', '', '', ''];
+let gameStatusId = ['', '', '', '', '', '', '', '', ''];
+let gamePlay = true;
+let currentPlayer = player1;
+let oponentPlayer = player2;
+
+const winningCombos = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+];
+
+let clickCount = 0;
+
+// Invert squares function
+
+const invertSquares = function () {
+
+  for (let i = 0; i < 9; i++) {
+    setTimeout(function() {
+      $(`#${i}`).addClass('square-invert');
+      $(`#${i}`).find('.content').addClass('content-invert');
+      $('.board').addClass('board-invert');
+    }, 25 * i);
   }
+};
 
-  let player2 = {
-    name: 'Player 2',
-    symbol: 'o',
-    score: 0
+// Draw mode - invert squares function alt
+
+const invertSquaresDraw = function () {
+
+  for (let i = 0; i < 9; i++) {
+    setTimeout(function() {
+      $(`#${i}`).addClass('square-invert-alt');
+      $(`#4`).find('.content').addClass('content-win').text(`Dangit. 🧐 It's a draw. Click to play again.`);;
+    }, 25 * i);
   }
+};
 
-  let gameStatus = ['', '', '', '', '', '', '', '', ''];
-  let gameStatusId = ['', '', '', '', '', '', '', '', ''];
-  let gamePlay = true;
-  let currentPlayer = player1;
-  let oponentPlayer = player2;
+// Reset squares function
 
-// Messages
-  const winningMessages = ['Cowabunga', 'Whoopee', 'Huzzah', 'w00t', 'Gnarly dude', 'Gee-whizz', 'Goshwow', 'Wicked'];
-  let randomNum = () => Math.round(Math.random() * 10);
+const resetSquares = function () {
+  for (let i = 0; i < 9; i++) {
+    setTimeout(function() {
+      $(`#${i}`).removeClass('square-invert square-invert-alt');
+      $(`#${i}`).find('.content').removeClass('content-win content-invert content-invert-alt');
+      $('.board').removeClass('board-invert');
+    }, 25 * i);
+  }
+};
 
-  const drawMessage = () => $('#message-board').text(`Would you look at that. It's a draw!`);
-  const nextPlayerTurn = (player) => $('#message-board').text(`It's your turn, ${player.name}!`);
-  const winMessage = function () {
-    $('#message-board').text(`${winningMessages[randomNum()]}! You won this round ${currentPlayer.name}!`);
+// Winner's squares message
 
+let winnerSq1;
+let winnerSq2;
+let winnerSq3;
 
+const winningSq = function (first, second, third) {
+  $(`#${first}`).addClass('square-invert').find('.content').addClass('content-invert content-win').text(`${winningMsg()}!`);
+  $(`#${second}`).addClass('square-invert').find('.content').addClass('content-invert content-win').text(`${currentPlayer.name}, you gosh darn won!`);
+  $(`#${third}`).addClass('square-invert').find('.content').addClass('content-invert content-win').text(`A point 4 u. Pls reset by clicking here.`);
+}
 
-  };
+//////////////////////////////// MESSAGES //////////////////////////////////////
 
-  nextPlayerTurn(currentPlayer);
+// Guidance message during game
+const nextPlayerTurn = (player) => $('#message-board').text(`It's your turn, ${player.name}!`);
 
-  const winningCombos = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-  ];
+nextPlayerTurn(currentPlayer);
 
-  let clickCount = 0;
+// Draw message
+const drawMessage = () => $('#message-board').text(`It's a draw so no points this round 😭`);
+
+// Winner's message
+const winningMessages = ['You bloody beauty', 'Whoopee', 'Huzzah', 'w00t', 'Gnarly dude', 'Gee-whizz', 'Goshwow', 'Wicked', 'Dangggg', 'Cowabunga'];
+
+const winningMsg = function () {
+  let randomIndex = Math.floor(Math.random() * winningMessages.length);
+  return winningMessages[randomIndex];
+}
+
+const winMessage = function () {
+  $('#message-board').text(`${winningMsg()}! You won this round ${currentPlayer.name}.`);
+  winningSq(winnerSq1, winnerSq2, winnerSq3);
+  invertSquares();
+};
 
 ///////////////////////////// GAME VALIDATION //////////////////////////////////
 
 const gameValidation = function () {
 
   let weHaveAWinner = false;
+  let draw = !gameStatus.includes('');
 
   for (let i = 0; i < winningCombos.length; i++) {
     const winSequence = winningCombos[i];
@@ -60,19 +119,16 @@ const gameValidation = function () {
     let b = [gameStatus[winSequence[1]], gameStatusId[winSequence[1]]];
     let c = [gameStatus[winSequence[2]], gameStatusId[winSequence[2]]];
 
-    console.log(`${a}, ${b}, ${c}\nBREAK`);
+    // console.log(`${a[0]}, ${b[0]}, ${c[0]}\nBREAK`);
 
     if (a[0] === '' || b[0] === '' || c[0] === '') {
       continue;
     }
 
-    if (clickCount === 9 && a[0] !== b[0] && b[0] !== c[0]) {
-      drawMessage();
-      gamePlay = false;
-      break
-    }
-
     if (a[0] === b[0] && b[0] === c[0]) {
+      winnerSq1 = a[1];
+      winnerSq2 = b[1];
+      winnerSq3 = c[1];
       weHaveAWinner = true;
       gamePlay = false;
       break
@@ -89,9 +145,16 @@ const gameValidation = function () {
     }
   }
 
+  else if (draw) {
+    drawMessage();
+    invertSquaresDraw();
+    gamePlay = false;
+    return;
+  }
+
   $('#p1-score').text(player1.score);
   $('#p2-score').text(player2.score);
-}
+};
 
 //////////////////////// FUNCTION IF SQUARE CLICKED ////////////////////////////
 
@@ -101,6 +164,7 @@ $('.square').on('click', function () {
 
 // If the click is even, add an 'x' - Player 1
   if (gamePlay === true && clickCount % 2 === 0 && $(`#${squareId}`).find('.content').text() !== player1.symbol && $(`#${squareId}`).find('.content').text() !== player2.symbol) {
+
     $(`#${squareId}`).find('.content').text(player1.symbol);
     gameStatus[squareId] = player1.symbol;
     gameStatusId[squareId] = squareId;
@@ -112,6 +176,7 @@ $('.square').on('click', function () {
 
 // If the click is even, add an 'o' - Player 2
   else if (gamePlay === true && clickCount % 2 !== 0 && $(`#${squareId}`).find('.content').text() !== player1.symbol && $(`#${squareId}`).find('.content').text() !== player2.symbol) {
+
     $(`#${squareId}`).find('.content').text(player2.symbol);
     gameStatus[squareId] = player2.symbol;
     gameStatusId[squareId] = squareId;
@@ -130,22 +195,25 @@ $('.square').on('click', function () {
 /////////////////////////////// RESET GAME /////////////////////////////////////
 
 // General reset game function
-let resetGame = function () {
-  gamePlay = true;
-  $(`.square`).find('.content').text('');
-  gameStatus = ['', '', '', '', '', '', '', '', ''];
-  currentPlayer = player1;
-  nextPlayerTurn(player1);
-  clickCount = 0;
-};
+  let resetGame = function () {
+    gamePlay = true;
+    $(`.square`).find('.content').text('');
+    gameStatus = ['', '', '', '', '', '', '', '', ''];
+    currentPlayer = player1;
+    nextPlayerTurn(player1);
+    clickCount = 0;
+    resetSquares();
+  };
 
 // Reset button click
-$('#reset').on('click', function () {
-  resetGame();
-});
+  $('#reset').on('click', function () {
+    resetGame();
+  });
 
 /////////////////////////// MISC STYLE FEATURES ////////////////////////////////
 
 // Board tilt function
 const tilt = $('.js-tilt').tilt()
 tilt.on('change', function(e, transforms){});
+
+// Entry sequence (tiles flicker to green then back to blue)
